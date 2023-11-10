ae1272df9fc3f3b21139e258d0a600a05591eb64
274188A <40014477+274188A@users.noreply.github.com>
2023-11-10 11:38:38 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using RestSharp;
+using System.Net;
+
+//https://api.test.datacite.org/dois
+
+var options = new RestClientOptions("https://api.test.datacite.org/dois");
+
+
+
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var configuration = builder.Build();
+
+
+
+
+var cred1 = new NetworkCredential()
+{
+    UserName = "john.barrett@curtin.edu.au",
+    Password = configuration["Password"]
+};
+
+
+//options.Credentials = new NetworkCredential();
+
+
+
+
+var client = new RestClient(options);
+var request = new RestRequest("");
+request.AddJsonBody("{\"data\":{\"type\":\"dois\",\"attributes\":{\"types\":{\"resourceTypeGeneral\":\"Audiovisual\"}}}}", false);
+var response = await client.PostAsync(request);
+
+Console.WriteLine("{0}", response.Content);
